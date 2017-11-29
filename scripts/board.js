912760e9db3460217ed54d110ab3279da2b97d90
Jonah Starling <starling.jonah@gmail.com>
2017-11-29 09:18:55 -0500

    Adding and Deleting Objects from the Database
    You can add and delete objects from the database. Currenlty there are some weird bugs with duplicates being created on the client side but it isn't happening on the backend so that is good.

--- scripts/board.js
+++ scripts/board.js
@@ -1,11 +1,68 @@
 //Used for testing purposes only
 var loremText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-var cardId = 1;
+var cardId = 0;
 var enlargeCardId;
 var enlargeCardType;
 var cardDragId;
 var cardObjects = [];
+
+function connectData() {
+	var ref = firebase.database().ref('cards/');
+	ref.on('value', function(snapshot) {
+	  updateCards(snapshot);
+	});
+}
+
+function updateCards(snapshot) {
+	console.log("update");
+	for (var key in snapshot.val()) {
+		var item = snapshot.val()[key];
+		console.log(key);
+		var card = getCardObjectById(key);
+		console.log(card);
+		if (card != false) {
+			card.updateCardType(item.cardType);
+			card.updateTitle(item.title);
+			card.updateDescription(item.description);
+			card.updateComments(item.comments);
+		} else {
+			var newCard = new CardObject(key, item.title, item.cardType, false);
+			newCard.updateDescription(item.description);
+			newCard.updateComments(item.comments);
+			cardObjects.push(newCard);
+		}
+	}
+	updateUI();
+}
+
+window.onload = connectData;
+
+function updateUI() {
+	// FIXME: ugly and inefficient
+	while (document.getElementById("greenCards").hasChildNodes()) {
+    document.getElementById("greenCards").removeChild(document.getElementById("greenCards").lastChild);
+	}
+	while (document.getElementById("yellowCards").hasChildNodes()) {
+    document.getElementById("yellowCards").removeChild(document.getElementById("yellowCards").lastChild);
+	}
+	while (document.getElementById("redCards").hasChildNodes()) {
+    document.getElementById("redCards").removeChild(document.getElementById("redCards").lastChild);
+	}
+	for (var i in cardObjects) {
+		switch(cardObjects[i].cardType) {
+			case "green":
+				addGreenCardToColumn(cardObjects[i]);
+				break;
+			case "yellow":
+				addYellowCardToColumn(cardObjects[i]);
+				break;
+			case "red":
+				addRedCardToColumn(cardObjects[i]);
+				break;
+		}
+	}
+}
 
 function getCardObjectById(id) {
 	var arrayLength = cardObjects.length;
@@ -14,48 +71,61 @@ function getCardObjectById(id) {
 	    	return cardObjects[i];
 	    }
 	}
+	return false;
 }
 
 function addGreenCard() {
-	cardId++;
-	var card = new CardObject(cardId, "Title");
+	cardId = cardObjects.length + 1;
+	var card = new CardObject(cardId, "Title", "green", true);
 	card.description = loremText;
 	cardObjects.push(card);
-	console.log(cardObjects);
-	var card = create("<div id=\""+cardId+"\" class=\"greenCard\" onclick=\"enlargeCard(\'green\',\'"+cardId+"\')\" draggable=\"true\" ondragstart=\"drag(event)\"><h1 class=\"titleText\">Title</h1><p class=\"descriptionText\">"+loremText+"</p></div>");
+	addGreenCardToColumn(card);
+}
+
+function addGreenCardToColumn(newCard) {
+	var card = create("<div id=\""+newCard.id+"\" class=\"greenCard\" onclick=\"enlargeCard(\'green\',\'"+newCard.id+"\')\" draggable=\"true\" ondragstart=\"drag(event)\"><h1 class=\"titleText\">"+newCard.title+"</h1><p class=\"descriptionText\">"+newCard.description+"</p></div>");
 	document.getElementById("greenCards").appendChild(card);
 }
 
 function deleteGreenCard(idToDelete) {
 	document.getElementById(""+idToDelete).remove();
+	firebase.database().ref('cards/'+idToDelete).remove();
 }
 
 function addYellowCard() {
-	cardId++;
-	var card = new CardObject(cardId, "Title");
+	cardId = cardObjects.length + 1;
+	var card = new CardObject(cardId, "Title", "yellow", true);
 	card.description = loremText;
 	cardObjects.push(card);
-	console.log(cardObjects);
-	var card = create("<div id=\""+cardId+"\" class=\"yellowCard\" onclick=\"enlargeCard(\'yellow\',\'"+cardId+"\')\" draggable=\"true\" ondragstart=\"drag(event)\"><h1 class=\"titleText\">Title</h1><p class=\"descriptionText\">"+loremText+"</p></div>");
+	addYellowCardToColumn(card);
+}
+
+function addYellowCardToColumn(newCard) {
+	var card = create("<div id=\""+newCard.id+"\" class=\"yellowCard\" onclick=\"enlargeCard(\'yellow\',\'"+newCard.id+"\')\" draggable=\"true\" ondragstart=\"drag(event)\"><h1 class=\"titleText\">"+newCard.title+"</h1><p class=\"descriptionText\">"+newCard.description+"</p></div>");
 	document.getElementById("yellowCards").appendChild(card);
 }
 
 function deleteYellowCard(idToDelete) {
 	document.getElementById(""+idToDelete).remove();
+	firebase.database().ref('cards/'+idToDelete).remove();
 }
 
 function addRedCard() {
-	cardId++;
-	var card = new CardObject(cardId, "Title");
+	cardId = cardObjects.length + 1;
+	var card = new CardObject(cardId, "Title", "red", true);
 	card.description = loremText;
 	cardObjects.push(card);
-	console.log(cardObjects);
-	var card = create("<div id=\""+cardId+"\" class=\"redCard\" onclick=\"enlargeCard(\'red\',\'"+cardId+"\')\" draggable=\"true\" ondragstart=\"drag(event)\"><h1 class=\"titleText\">Title</h1><p class=\"descriptionText\">"+loremText+"</p></div>");
+	addRedCardToColumn(card);
+}
+
+function addRedCardToColumn(newCard) {
+	var card = create("<div id=\""+newCard.id+"\" class=\"redCard\" onclick=\"enlargeCard(\'red\',\'"+newCard.id+"\')\" draggable=\"true\" ondragstart=\"drag(event)\"><h1 class=\"titleText\">"+newCard.title+"</h1><p class=\"descriptionText\">"+newCard.description+"</p></div>");
 	document.getElementById("redCards").appendChild(card);
 }
 
 function deleteRedCard(idToDelete) {
 	document.getElementById(""+idToDelete).remove();
+	firebase.database().ref('cards/'+idToDelete).remove();
 }
 
 function enlargeCard(type, id) {
@@ -144,7 +214,7 @@ function drop(ev) {
 var editing  = false;
 var title = false;
 var comment = false;
-var butt;
+var butt; // Wait to initialize the butt
 var commentEditId;
 
 if (document.getElementById && document.createElement) {
@@ -156,11 +226,9 @@ if (document.getElementById && document.createElement) {
 
 function catchIt(e) {
 	if (editing) {
-		console.log("editing");
 		return;
 	}
 	if (!document.getElementById || !document.createElement) {
-		console.log("doc");
 		return;
 	}
 	if (!e) var obj = window.event.srcElement;
@@ -204,6 +272,7 @@ function saveEdit() {
 		var y = document.createElement('H1');
 		y.className += " largeCardTitle";
 		document.getElementById(enlargeCardId).getElementsByTagName("h1")[0].innerHTML = area.value;
+		getCardObjectById(enlargeCardId).title = area.value;
 	} else if (comment) {
 		var y = document.createElement('P');
 		y.className += "commentText";
@@ -212,6 +281,7 @@ function saveEdit() {
 		var y = document.createElement('P');
 		y.className += " largeCardDescription"
 		document.getElementById(enlargeCardId).getElementsByTagName("p")[0].innerHTML = area.value;
+		getCardObjectById(enlargeCardId).description = area.value;
 	}
 	var z = area.parentNode;
 	y.addEventListener("click", catchIt)
@@ -224,22 +294,58 @@ function saveEdit() {
 
 class CardObject {
 
-	constructor(id, title) {
+	constructor(id, title, cardType, update) {
 		this.id = id;
+		this.cardType = cardType;
 		this.title = title;
 		this.description = "";
 		this.comments = ["test comment 1", "test comment 2", "test comment 3"];
+		if (update) {
+			firebase.database().ref('cards/' + id).set({
+				cardType: this._cardType,
+				title: this._title,
+				description: this._description,
+				comments: this._comments
+			});
+		}
 	}
 
 	get id() { return this._id; }
+	get cardType() { return this._cardType; }
 	get title() { return this._title; }
 	get description() { return this._description; }
 	get comments() { return this._comments;	}
 
 	set id(newId) { this._id = newId; }
-	set title(newTitle) { this._title = newTitle; }
-	set description(newDescription) { this._description = newDescription; }
-	set comments(newComments) { this._comments = newComments; }
+	set cardType(newCardType) {
+		this._cardType = newCardType;
+		firebase.database().ref('cards/' + this._id).update({cardType: newCardType});
+	}
+	set title(newTitle) {
+		this._title = newTitle;
+		firebase.database().ref('cards/' + this._id).update({title: newTitle});
+	}
+	set description(newDescription) {
+		this._description = newDescription;
+		firebase.database().ref('cards/' + this._id).update({description: newDescription});
+	}
+	set comments(newComments) {
+		this._comments = newComments;
+		firebase.database().ref('cards/' + this._id).update({comments: newComments});
+	}
+
+	updateCardType(newCardType) {
+		this._cardType = newCardType;
+	}
+	updateTitle(newTitle) {
+		this._title = newTitle;
+	}
+	updateDescription(newDescription) {
+		this._description = newDescription;
+	}
+	updateComments(newComments) {
+		this._comments = newComments;
+	}
 
 	addComment(comment) {
 		this._comments.push(comment);
